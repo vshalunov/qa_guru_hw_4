b7cc15aa77a0b1768cb03c04915ae4024a955a56
zlw-qa <asdori95@gmail.com>
2021-11-05 20:22:22 +0400

    added dragAndDropTest

--- src/test/java/com/zlwqa/github/DragAndDropTest.java
+++ src/test/java/com/zlwqa/github/DragAndDropTest.java
@@ -1,4 +1,29 @@
 package com.zlwqa.github;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class DragAndDropTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void dragAndDropTest(){
+        // Откройте https://the-internet.herokuapp.com/drag_and_drop
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        // Перенесите прямоугольник А на место В
+        $("#column-a").dragAndDropTo($("#column-b"));
+        // Проверьте, что прямоугольники действительно поменялись
+        $("#column-b").shouldHave(text("A"));
+        $("#column-a").shouldHave(text("B"));
+    }
+
 }
